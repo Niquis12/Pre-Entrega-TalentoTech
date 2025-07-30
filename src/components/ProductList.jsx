9f3124fe6d55b8799ed96584cdab1e3a913b9f13
Laureano Rojas <laureanorojas425@gmail.com>
2025-07-29 21:33:55 -0300

    solucionImagen

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Products from './Products'
+import './styleProductList.css'
 
 const ProductList = ({productos, agregarCarrito}) => {
   return (
     <>
         <h2>Galeria de productos</h2>
-        <div style={{display:"flex", flexWrap:"wrap", justifyContent:"space-evenly"}}>
+        <div className='cardConteiner'>
             {/* hola como va */}
           {
             productos.map(productos => (
